95724ee7ba24720d20c53ea9a946725e6a9fb5bc
Dickson-Onyongo <dicksononyong@gmail.com>
2024-03-13 15:02:49 -0700

    to cut some charracters while printing

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 j = ' '.join(chr(i)for i in range(97, 123))
-print("{}".format(j), end=' ')
+print("{}".format(j),)
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+for x in range(97, 123):
+    if chr(x) != 'q' and chr(x) != 'e':
+        print("{}".format(chr(x)), end=' ')
