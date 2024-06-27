939e78329b12f451a65cadac794f98cd633e8a07
Dickson-Onyongo <dicksononyong@gmail.com>
2024-06-28 01:23:04 +0300

    create DB,and user and grant select privileges

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,9 +2,9 @@
 -- User_0d_2 should have only SELECT privilege in the database hbtn_0d_2
 -- The user_0d_2 password should be set to user_0d_2_pwd
 -- If the database hbtn_0d_2 already exists, your script should not fail
--- If the user user_0d_2 already exists, your script should not fail
 
-CREATE IF NOT EXIST DATABASE hbtn_0d_2;
-CREATE IF NOT EXIST USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT TO 'hbtn_0d_2'@'localhost' WITH GRANT OPTION;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON 'hbtn_0d_2'.* TO 'hbtn_0d_2'@'localhost';
+FLUSH PRIVILEGES;
 
