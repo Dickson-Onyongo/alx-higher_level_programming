8ff4c661461528061316ac3028f9647eb11d3762
Dickson-Onyongo <dicksononyong@gmail.com>
2024-07-15 23:37:05 +0300

    post email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+"""
+script that takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter, and displays
+the body of the response (decoded in utf-8)
+
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    data = urllib.parse.urlencode({'email': sys.arg[2]}).encode('ascii')
+    req = urllib.request.Reqest(sys.argv[1], data)
+    with urllib.request.urlopen(req) as r:
+        html = r.read()
+        print('Your email is: {}'.format(html.decode('utf-8')))
