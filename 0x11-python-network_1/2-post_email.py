3cda396b4fc4fa26f37304a0b116811e76e08c3e
Dickson-Onyongo <dicksononyong@gmail.com>
2024-07-15 14:27:35 -0700

    post email update

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """
 script that takes in a URL and an email, sends a POST request
 to the passed URL with the email as a parameter, and displays
@@ -10,9 +10,9 @@ import urllib.parse
 import sys
 
 if __name__ == "__main__":
-    data = urllib.parse.urlencode({'email': sys.arg[2]}).encode('ascii')
-    req = urllib.request.Reqest(sys.argv[1], data
-)
+    data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('ascii')
+    req = urllib.request.Request(sys.argv[1], data)
+
     with urllib.request.urlopen(req) as response:
         response = response.read()
-        print('Your email is: {}'.format(response.decode('utf-8')))
+        print('{}'.format(response.decode('utf-8')))
