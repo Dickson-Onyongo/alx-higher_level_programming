6124a97a93571cd9b5358df9f7e041e947ccdc4c
Dickson-Onyongo <dicksononyong@gmail.com>
2024-07-15 10:44:54 +0300

    methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# display all HTTP methods the server will accept using curl.
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+# Script to Display all HTTP methods the server will accept using curl.
+curl -sI "$1" | grep Allow | cut -f2- -d' '
 
