013f22a98b27ee058676cd2906278a6efbe8922d
Dickson-Onyongo <dicksononyong@gmail.com>
2024-08-10 17:17:09 +0300

    JQuery work

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,5 @@
+$(document).ready(functon () {
+    $("#red_header").click(function() {
+        $("header").addClass("red");
+    });
+});
---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("DIV#toggle_header").click(function() {
+
+    $("header").toggleClass("red green");
+    });
+  
+});
+
